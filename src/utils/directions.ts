47ae67446f00dd4aa8cf3cecc18f328d627e9deb
The-Jackrabbit <lsm5fm@virginia.edu>
2022-10-22 12:44:51 -0400

    more cleanup

--- src/utils/directions.ts
+++ src/utils/directions.ts
@@ -2,12 +2,12 @@ export function isMoreVerticalThanHorizontal(mx: number, my: number) {
   const x = mx ? Math.abs(mx) : 0;
   const y = my ? Math.abs(my) : 0;
  
-  return y > 2*x;
+  return y > 5*x;
 }
 
 export function isMoreHorizontalThanVertical(mx: number, my: number) {
   const x = mx ? Math.abs(mx) : 0;
   const y = my ? Math.abs(my) : 0;
  
-  return x > 2*y;
+  return x > 5*y;
 }
