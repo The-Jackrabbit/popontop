4cae6d9fa4e6e29195b593590a18b984e8f8c169
The-Jackrabbit <lsm5fm@virginia.edu>
2022-10-25 16:56:01 -0700

    fix build

--- src/components/global/DesktopEditor/Sidebar/Settings/DownloadButton.tsx
+++ src/components/global/DesktopEditor/Sidebar/Settings/DownloadButton.tsx
@@ -4,33 +4,26 @@ import dynamic from 'next/dynamic'
 import * as htmlToImage from 'html-to-image';
 const DownloadButton: React.FC = () => {
   const downloadChart = (): void => {
-
-    
-    function filter(node) {
-      // return node.tagName !== "i";
-    }
     if (window) {
-      
       const chart = document.getElementById('mobile-chart');
+
+      if (!chart) {
+        return;
+      }
+
       htmlToImage.toPng(chart, {
-        style: {
-          // backgroundColor: 'red',
-        },
         cacheBust: true,
       })
         .then(function (dataUrl) {
           const link = document.createElement('a');
-    link.download = 'my-image-name.png';
-    link.href = dataUrl;
-    link.click();
+          link.download = 'my-image-name.png';
+          link.href = dataUrl;
+          link.click();
         })
         .catch(function (error) {
           console.error('oops, something went wrong!', error);
         });
     }
-
-
- 
   }
 
   return (
