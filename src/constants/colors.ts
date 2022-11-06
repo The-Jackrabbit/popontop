e53ca1c9b84c6866636d0e74047a7a86c29d48ee
The-Jackrabbit <lsm5fm@virginia.edu>
2022-11-05 22:49:19 -0700

    small style tweaks

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -6,6 +6,10 @@ export const colorMap = {
   purple: 'bg-purple-400 border-purple-400 hover:bg-purple-300 active:bg-purple-200 shadow-purple-500',
   violet: 'bg-violet-400 border-violet-400 hover:bg-violet-300 active:bg-violet-200 shadow-violet-500',
   fuchsia: 'bg-fuchsia-400 border-fuchsia-400 hover:bg-fuchsia-300 active:bg-fuchsia-200 shadow-fuchsia-500 dark:shadow-fuchsia-500',
-  neutral: 'hover:bg-neutral-200 active:bg-neutral-100\
-  shadow-neutral-400 dark:shadow-neutral-600 bg-neutral-300 dark:bg-neutral-700 border-neutral-300'
+  neutral: `
+    bg-neutral-300 dark:bg-neutral-800
+    hover:bg-neutral-200 dark:hover:bg-neutral-700
+    shadow-neutral-400 dark:shadow-neutral-600
+    border-neutral-300 dark:border-neutral-600
+  `
 }
