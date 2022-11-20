ac25f98df483c20205a91db6084f994afc5c38c7
The-Jackrabbit <lsm5fm@virginia.edu>
2022-11-20 14:30:14 -0800

    fix theme color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+        
         <link rel="manifest" href="./site.webmanifest" />
         <meta
           name="description"
@@ -25,13 +26,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
             Popontop lets you create and customize lists of your favorite music.
           `}
         />
-        <meta name="theme-color" content="var(--theme)" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#f8fafc"
+        />
         <meta
           name="theme-color"
           media="(prefers-color-scheme: dark)"
           content="#171717"
         />
-        {ismobile() && false && (
+        {ismobile() && (
           <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale = 1.0, initial-scale=1.0, viewport-fit=cover" />
         )}
         <title>popontop</title>
