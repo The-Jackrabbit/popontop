ecd9395c3f57efaff4d8044ca6eca80c35674e9e
The-Jackrabbit <lsm5fm@virginia.edu>
2022-11-25 19:02:18 -0800

    sliders for  view mode

--- src/components/global/MobileEditor/ViewModeModal/ViewModeModal.tsx
+++ src/components/global/MobileEditor/ViewModeModal/ViewModeModal.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import { Album } from "../../../../styles/types/Albums";
 import Grid from "../../../lib/Grid/Grid";
-import NumberInput from "../../../lib/NumberInput/NumberInput";
+import NumberInput, { useIncrementer } from "../../../lib/NumberInput/NumberInput";
 
 export interface Props {
   list: Album[];
 }
 export const ViewModeModal: React.FC<Props> = ({ list }) => {
-  const [numberOfColumns, setNumberOfColumns] = useState(5);
-
-  
-  const onDecrement = () => setNumberOfColumns(numberOfColumns - 1);
-  const onIncrement = () => setNumberOfColumns(numberOfColumns + 1);
+  const [rows, onIncrementRows, onDecrementRows] = useIncrementer({ });
+  const [columns, onIncrementColumns, onDecrementColumns] = useIncrementer({ }); 
   return (
     <div
       className="
@@ -21,13 +17,25 @@ export const ViewModeModal: React.FC<Props> = ({ list }) => {
     >
       <NumberInput
         className="flex w-full justify-between mb-4"
-        currentValue={numberOfColumns}
+        currentValue={rows}
         label="number of rows"
-        onDecrement={onDecrement}
-        onIncrement={onIncrement}
+        onDecrement={onDecrementRows}
+        onIncrement={onIncrementRows}
       />
 
-      <Grid list={list} columns={numberOfColumns} />
+      <NumberInput
+        className="flex w-full justify-between mb-4"
+        currentValue={columns}
+        label="number of columns"
+        onDecrement={onDecrementColumns}
+        onIncrement={onIncrementColumns}
+      />
+
+      <Grid
+        list={list}
+        columns={columns}
+        rows={rows}
+      />
     </div>
   )
 };
--- src/components/lib/Grid/Grid.tsx
+++ src/components/lib/Grid/Grid.tsx
@@ -5,42 +5,53 @@ import { useEffect, useState } from "react";
 export interface Props {
   columns: number;
   list: Album[];
+  rows: number;
 }
 
 const Grid: React.FC<Props> = ({
   columns,
   list,
+  rows,
 }) => {
   const [squareWidth, setSquareWidth] = useState(40);
   useEffect(() => {
     const container = document.getElementById('container');
     if (container?.clientWidth) {
-      const containerWidth = container?.clientWidth;
-      setSquareWidth(Math.floor(containerWidth/columns)-1);
+      const containerWidth = container?.clientWidth ?? 0;
+      const containerHeight = container?.clientHeight ?? 0;
+      setSquareWidth(
+        Math.floor(
+          Math.min(
+            containerWidth/columns - 2, 
+            containerHeight/rows - 2,
+          )
+        )
+      );
     }
-  }, [columns]);
+  }, [columns, rows]);
   
   return (
     <div
       id="container"
       className="
         flex flex-wrap justify-center
-        max-h-[80vh] w-full
+        h-[80vh] w-full
         overflow-y-scroll
+        content-center 
       "
-
     >
-      {list.map((album, index) => (
-        <div 
-          className="relative"
-          key={index+'gfrdi'}
-          style={{ width: `${squareWidth}px`, height: `${squareWidth}px` }}
-        >
-          <Image
-            layout='fill'
-            src={album.imageUrl}
-            alt={album.artist}
-          />
+          {[...new Array(columns)].map((_, index) => (
+            <div className="row" key={'row'+index}>
+          {[...new Array(rows)].map((_, cindex) => (
+            <div className="value" key={'row'+index+cindex}
+            style={{ width: squareWidth, height: squareWidth}}
+            >
+              <img
+                src={list[cindex+(rows*index)]?.imageUrl ?? ''}
+                alt={list[cindex+(rows*index)]?.artist ?? ''}
+              />              
+            </div>
+          ))}
         </div>
       ))}
     </div>
